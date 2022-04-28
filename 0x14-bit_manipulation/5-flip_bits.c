5fa796ffbb27658e9b51d723cb9cc3700210cbcb
obelr <debebe.robel@gmail.com>
2022-04-28 19:50:16 +0300

    My commit bit manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,17 +8,12 @@
  *
  * Return: 1 if it worked, -1 if it didn't.
  */
-int clear_bit(unsigned long int *n, unsigned int index)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int m;
+	int shift = (sizeof(n) * BYTE_LENGHT);
+	int dist = 0;
 
-	if (index > 63)
-		return (-1);
-
-	m = 1 << index;
-
-	if (*n & m)
-		*n ^= m;
-
-	return (1);
+	while (shift--)
+		dist += (n >> shift & 1) != (m >> shift & 1);
+	return (dist);
 }
